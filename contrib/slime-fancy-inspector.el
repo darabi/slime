d27f2a09224f14fd7a341c2448218ea4d903b76d
Attila Lendvai <attila.lendvai@gmail.com>
2010-10-12 22:25:12 +0200

    slime-inspect got a :mode argument that controls its behavior.
    Most notably with :mode :dwim (default) it'll try to find out the user's
    intention (e.g. look up definitions when a symbol is inspected). To avoid
    annoyance at misses, it puts the original object on the inspector stack,
    so that 'l' brings back to the non-dwim value.
    
    Also add an *inspector-lookup-hooks* customization point.

--- contrib/slime-fancy-inspector.el
+++ contrib/slime-fancy-inspector.el
@@ -5,7 +5,7 @@
   "Fancy inspector for CLOS objects."
   (:authors "Marco Baringer <mb@bese.it> and others")
   (:license "GPL")
-  (:slime-dependencies slime-parse)
+  (:slime-dependencies slime-parse slime-presentations)
   (:swank-dependencies swank-fancy-inspector)
   (:on-load
    (add-hook 'slime-edit-definition-hooks 'slime-edit-inspector-part))
@@ -38,5 +38,48 @@
                                        :location location))
                 name
                 where)))))))
+
+;; redefine the original
+(defun* slime-inspect (form &key (reset t) (mode :dwim) thread
+                            (package (slime-current-package)))
+  "Take an expression FORM and inspect it.
+
+The MODE argument controls behavior:
+ - :dwim try to be smart about what was the user's intention
+ - :eval evaluate FORM and inspect the result
+ - :as-is inspect FORM"
+  (interactive
+   (multiple-value-bind (presentation start end)
+       (slime-presentation-around-point (point))
+     (if presentation
+         ;; Point is within a presentation, so don't prompt, just
+         ;; inspect the presented object; don't play DWIM.
+         (cons (slime-presentation-expression presentation)
+               '(:mode :eval))
+         ;; Not in a presentation, read form from minibuffer.
+         (cons (let ((sexp (slime-sexp-at-point)))
+                 (if (and sexp
+                          ;; an string with alphanumeric chars and hyphens only?
+                          (and (string-match "\\([-|*.:0-9a-zA-Z]*\\)" sexp)
+                               (= (match-end 0) (length sexp))))
+                     sexp
+                     (slime-read-from-minibuffer (format (if current-prefix-arg
+                                                             "Inspect value (evaluated in %s): "
+                                                             "Inspect value (dwim mode in %s): ")
+                                                         (slime-current-package))
+                                                 sexp)))
+               (if current-prefix-arg
+                   '(:mode :eval)
+                   '(:mode :dwim))))))
+  (slime-eval-async `(swank:init-inspector ,form
+                                           :reset ,reset
+                                           :mode ,mode)
+                    (lexical-let ((thread thread)
+                                  (package package)
+                                  (form form))
+                      (lambda (thing)
+                        (if thing
+                            (slime-open-inspector thing nil nil thread package)
+                            (message "Couldn't read anything from '%s' (hint: prefix for debugger with details)" form))))))
 
 (provide 'slime-fancy-inspector)
